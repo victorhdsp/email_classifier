7607199a6f9956d3082a32809f06c4a7cc00f920
victorhdsp <victorhugods.pereira@gmail.com>
2025-07-03 13:47:18 -0300

    fix: :bug: download spacy model

--- backend/src/shared/infra/spacy_npl.py
+++ backend/src/shared/infra/spacy_npl.py
@@ -1,11 +1,16 @@
+from os import path
+
 import spacy
 
 from src.shared.services.nlp import NLPInterfaceAdapter, Token
+from src.shared.utils.path import get_project_root
 
 
 class SpacyNLPAdapter(NLPInterfaceAdapter):
     def __init__(self, nlp_model: str = "pt_core_news_sm"):
-        self.nlp = spacy.load(nlp_model)
+        self.nlp = spacy.load(
+            path.join(f"{get_project_root()}/spacy_models/{nlp_model}")
+        )
 
     def extract_token(self, text: str) -> list[Token]:
         doc = self.nlp(text)
