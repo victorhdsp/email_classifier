b5204086dd1b35a13571108863298971764ee048
victorhdsp <victorhugods.pereira@gmail.com>
2025-07-06 15:47:14 -0300

    docs: :memo: add documents decisions

--- backend/src/routes/v1/route.py
+++ backend/src/routes/v1/route.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from src.routes.health_doc import health_check_doc
 from src.routes.v1.analyze.route import analyze_router
 
-v1_router = APIRouter(prefix="/v1")
+v1_router = APIRouter(prefix="/v1", tags=["Versão 1"])
 
 
 @v1_router.get("/health", **health_check_doc)
